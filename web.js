9f67ecdebc4e699cffeefa3b0a3bd9f63a66f87d
Pascal Bois <pascal.bois@gmail.com>
2013-07-10 21:46:02 -0400

    HW3 part 1

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  var buffer = new Buffer(fs.readFileSync('index.html'), "ascii");
+  response.send(buffer.toString("ascii"));
 });
 
 var port = process.env.PORT || 5000;
